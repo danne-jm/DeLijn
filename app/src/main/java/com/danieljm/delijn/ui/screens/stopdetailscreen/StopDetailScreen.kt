6b0d96278a1f147bb64ded8a0654e5e1d4392d48
daniel <daniel.jaurellmevorach@student.ucll.be>
2025-09-18 13:34:34 +0200

    non blockong of stop on stop detail screen map by bottom sheet

--- app/src/main/java/com/danieljm/delijn/ui/screens/stopdetailscreen/StopDetailScreen.kt
+++ app/src/main/java/com/danieljm/delijn/ui/screens/stopdetailscreen/StopDetailScreen.kt
@@ -2,10 +2,8 @@ package com.danieljm.delijn.ui.screens.stopdetailscreen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
@@ -20,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
